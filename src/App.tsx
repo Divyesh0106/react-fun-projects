110fb13ec1cd02256c57a3304b29f0220e340389
Divyesh0106 <bdivyesh.gd@gmail.com>
2024-01-19 13:10:15 +0530

    Route Delay test function remove

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,11 @@ import React, { lazy, Suspense } from 'react';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-function delay(Component:any):any{
-  return new Promise( resolve =>{
-    setTimeout(()=>{
-      resolve(Component);
-    },5000);
-  })
-}
-
 const Loader = lazy(()=> import('./Layout/loader'));
 const Layout  = lazy(() => import('./Layout/layout'));
 const MainPage = lazy(() =>import('./Layout/mainPage'));
 const TicTacToe = lazy(() => import('./TicTacToe/Tictactoe'));
-const Clock25 = lazy(() => delay(import('./Clock25/Clock25')));
+const Clock25 = lazy(() => import('./Clock25/Clock25'));
 const Calculator = lazy(() => import('./Calculator/Calculator'));
 const DrumMachine = lazy(() => import('./Drummachine/Drummachine'));
 const MarkdownPreviwer = lazy(() => import('./MarkdownPreviwer/MarkdownPreviwer'));
